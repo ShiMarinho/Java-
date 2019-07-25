57656088fb8e7c0353da459974b9736e12edf1e0
ShiMarinho <profa.shirley.marinho@gmail.com>
2019-07-25 19:52:29 +0100

    the validate() and repaint() where replace by by runnable at main class. It does the same work but also guarantee that the program runs only all the resources ready to display.

---
+++ src/hospitalManagementSystem/HospitalManagementSystem.java
@@ -0,0 +1,31 @@
+/*
+ * DATA STRUCTURE and ALGOTITHMS
+ * CA - Hospital Patient Management List
+ * Assignment compiler: Dr. Muhammad Iqbal
+ * Due Date: 21/12/2018 
+ * Method of Submission: Moodle Submission
+ * 
+ */
+package hospitalManagementSystem;
+
+/**
+ * Hospital Management System is a main class which uses an instance of Menu
+ * class to display the menu options
+ *
+ * @author ms
+ */
+public class HospitalManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        /*
+         * Menu Class 
+         */
+        new Menu();
+
+    }
+
+}
